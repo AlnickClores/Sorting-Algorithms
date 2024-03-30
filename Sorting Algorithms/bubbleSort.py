e9d95a97127889773b8a36a51d6d70a7b6a17f6a
Alnick <91898057+AlnickClores@users.noreply.github.com>
2024-03-31 00:07:07 +0800

    Update bubbleSort.py

--- Sorting Algorithms/bubbleSort.py
+++ Sorting Algorithms/bubbleSort.py
@@ -2,9 +2,12 @@
 
 numList = [4,5,3,1,2]
 
-for i in range(len(numList)):
-    for j in range(len(numList) - 1):
-        if numList[j] > numList[j + 1]:
-            numList[j + 1], numList[j] = numList[j], numList[j + 1]
+def bubbleSort(numList):
+    for i in range(len(numList)):
+        for j in range(len(numList) - 1):
+            if numList[j] > numList[j + 1]:
+                numList[j + 1], numList[j] = numList[j], numList[j + 1]
+    return numList
 
-print(numList)
+sortedList = bubbleSort(numList)
+print(sortedList)
